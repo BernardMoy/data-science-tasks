0fb7e7160d812720b3aaa39d22a831ea5fefcf09
BernardMoy <bernardmoy3@gmail.com>
2025-07-04 00:22:27 +0800

    Month by year plot but with numbered months

--- boilerplate-page-view-time-series-visualizer/time_series_visualizer.py
+++ boilerplate-page-view-time-series-visualizer/time_series_visualizer.py
@@ -6,7 +6,9 @@ from pandas.plotting import register_matplotlib_converters
 register_matplotlib_converters()
 
 # Import data (Make sure to parse dates. Consider setting index column to 'date'.)
-df = pd.read_csv("fcc-forum-pageviews.csv", index_col="date", parse_dates=True)
+df = pd.read_csv(
+    "fcc-forum-pageviews.csv", index_col="date", parse_dates=True
+)  # Parse as dates
 print(df.columns)
 
 # Clean data
@@ -19,7 +21,7 @@ df = df[
 def draw_line_plot():
     # Draw line plot
     fig, ax = plt.subplots(figsize=(12, 6))
-    ax.plot(df.index, df["value"])
+    ax.plot(df.index, df["value"], color="red")
     ax.set_xlabel("Date")
     ax.set_ylabel("Page Views")
     ax.set_title("Daily freeCodeCamp Forum Page Views 5/2016-12/2019")
@@ -31,9 +33,20 @@ def draw_line_plot():
 
 def draw_bar_plot():
     # Copy and modify data for monthly bar plot
-    df_bar = None
+    df_bar = df.copy()
+    df_bar["year"] = df.index.year
+    df_bar["month"] = df.index.month
+    df_bar = df_bar.groupby(["year", "month"])
+
+    df_grouped = df_bar["value"].mean().reset_index(name="avg")
+
+    pivoted_df = df_grouped.pivot(columns="month", values="avg", index="year").fillna(0)
+
+    print(pivoted_df.head())
 
     # Draw bar plot
+    fig, ax = plt.subplots(figsize=(12, 6))
+    pivoted_df.plot(kind="bar", stacked=False, ax=ax)
 
     # Save image and return fig (don't change this part)
     fig.savefig("bar_plot.png")
