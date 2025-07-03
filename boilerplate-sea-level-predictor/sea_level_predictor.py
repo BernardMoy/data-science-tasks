7a0954cec215399f821548bf92b4b1f4036589a0
BernardMoy <bernardmoy3@gmail.com>
2025-07-04 01:37:54 +0800

    Plot 2 best fit lines

--- boilerplate-sea-level-predictor/sea_level_predictor.py
+++ boilerplate-sea-level-predictor/sea_level_predictor.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 from scipy.stats import linregress
+import numpy as np
 
 
 def draw_plot():
@@ -14,8 +15,27 @@ def draw_plot():
     )
 
     # Create first line of best fit
+    slope, intercept, r, p, se = linregress(df["Year"], df["CSIRO Adjusted Sea Level"])
+
+    # Line space is from 1880 to 2050
+    x_range = np.array([1880, 2050])
+    y_range = np.array([slope * i + intercept for i in x_range])
+
+    plt.plot(x_range, y_range, color="blue")
 
     # Create second line of best fit
+
+    # Filter dataframe to only consider data after year 2000
+    df_after_2000 = df.loc[df["Year"] >= 2000]
+    slope2, intercept2, r2, p2, se2 = linregress(
+        df_after_2000["Year"], df_after_2000["CSIRO Adjusted Sea Level"]
+    )
+
+    # Line space is from 2000 to 2050
+    x_range2 = np.array([2000, 2050])
+    y_range2 = np.array([slope2 * i + intercept2 for i in x_range2])
+
+    plt.plot(x_range2, y_range2, color="red")
 
     # Add labels and title
     plt.xlabel("Year")
