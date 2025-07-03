cad05e4f8c71fd58fead58e143c8e8cfa9976230
BernardMoy <bernardmoy3@gmail.com>
2025-07-03 15:16:49 +0800

    Plotting the figure

--- boilerplate-medical-data-visualizer/medical_data_visualizer.py
+++ boilerplate-medical-data-visualizer/medical_data_visualizer.py
@@ -13,21 +13,36 @@ df["overweight"] = (df["weight"] / (df["height"] * df["height"]) > 25).astype(in
 df["cholesterol"] = (df["cholesterol"] > 1).astype(int)
 df["gluc"] = (df["gluc"] > 1).astype(int)
 
-print(df.head())
-
 
 # 4
 def draw_cat_plot():
     # 5
-    df_cat = None
+    df_cat = pd.melt(
+        df,
+        id_vars=["cardio"],  # To keep
+        value_vars=[
+            "cholesterol",
+            "gluc",
+            "smoke",
+            "alco",
+            "active",
+            "overweight",
+        ],  # To melt
+        var_name="variable",  # New name
+        value_name="value",  # Values of the value_vars
+    )
 
     # 6
-    df_cat = None
+    df_cat = df_cat.groupby(["cardio", "variable", "value"]).size()  # Grouped keys
+    df_cat = df_cat.reset_index(name="total")
 
     # 7
+    fig = sns.catplot(
+        data=df_cat, x="variable", y="total", col="cardio", hue="value", kind="bar"
+    )
 
     # 8
-    fig = None
+    # plt.show()
 
     # 9
     fig.savefig("catplot.png")
