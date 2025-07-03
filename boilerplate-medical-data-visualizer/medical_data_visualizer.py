7cd673772ac8eca391b668080cc128b1ca944374
BernardMoy <bernardmoy3@gmail.com>
2025-07-03 12:17:41 +0800

    Up to before plot

--- boilerplate-medical-data-visualizer/medical_data_visualizer.py
+++ boilerplate-medical-data-visualizer/medical_data_visualizer.py
@@ -4,12 +4,16 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # 1
-df = None
+df = pd.read_csv("medical_examination.csv")
 
 # 2
-df['overweight'] = None
+df["overweight"] = (df["weight"] / (df["height"] * df["height"]) > 25).astype(int)
 
 # 3
+df["cholesterol"] = (df["cholesterol"] > 1).astype(int)
+df["gluc"] = (df["gluc"] > 1).astype(int)
+
+print(df.head())
 
 
 # 4
@@ -17,21 +21,16 @@ def draw_cat_plot():
     # 5
     df_cat = None
 
-
     # 6
     df_cat = None
-    
 
     # 7
-
-
 
     # 8
     fig = None
 
-
     # 9
-    fig.savefig('catplot.png')
+    fig.savefig("catplot.png")
     return fig
 
 
@@ -46,15 +45,11 @@ def draw_heat_map():
     # 13
     mask = None
 
-
-
     # 14
     fig, ax = None
 
     # 15
 
-
-
     # 16
-    fig.savefig('heatmap.png')
+    fig.savefig("heatmap.png")
     return fig
